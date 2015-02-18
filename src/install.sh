6a39f3f650131d3bfaebe5cec38886f8ce274415
mdelaney68 <mdelaney@digiconasp.com>
2015-02-18 12:37:14 -0500

    Update Installer

--- src/install.sh
+++ src/install.sh
@@ -8,12 +8,11 @@ sudo apt-get install unzip
 if [ ! -f "railo-4.2.1.008-pl0-linux-x64-installer.run" ]
 then
 	sudo wget -O railo-4.2.1.008-pl0-linux-x64-installer.run  http://www.getrailo.org/down.cfm?item=/railo/remote/download42/4.2.1.008/tomcat/linux/railo-4.2.1.008-pl0-linux-x64-installer.run&thankyou=true
-
 fi
 
-if [ ! -f "DigiconDemo.zip" ]
+if [ ! -f "Demo-master.zip" ]
 then
-	sudo wget -O DigiconDemo.zip  http://officedev.digicon.com/deployment/DigiconDemo.zip
+	sudo wget -O Demo-master.zip  https://github.com/mdelaney68/Demo/archive/master.zip
 fi
 
 sudo chmod 777 railo-4.2.1.008-pl0-linux-x64-installer.run
@@ -25,9 +24,12 @@ echo "  Please Wait!! - This may take several minutes"
 echo "==================="
 sudo ./railo-4.2.1.008-pl0-linux-x64-installer.run --mode unattended --optionfile railoinstall.txt
 sudo chmod 777 /opt/railo/ -R
-# install DigiconDemo into Railo website root
-echo "Installing DigiconDemo "
-sudo unzip -o DigiconDemo.zip  -d /opt/railo/tomcat/webapps/ROOT/
+# install Demo-master into Railo website root
+echo "Installing Demo-master "
+# sudo unzip -o Demo-master.zip  -d /opt/railo/tomcat/webapps/ROOT/
+sudo unzip -o Demo-master.zip  -d ./tmp
+sudo cp -r ./tmp/Demo-master/src/wwwroot /opt/railo/tomcat/webapps/ROOT/
+sudo rm ./tmp -R
 echo "==================="
 echo "  Install Complete "
 echo "Access http://localhost:8888/ to view the application"
