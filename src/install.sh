7acc6c420e39f813b610fdee8e7e7b38ab990ecb
mdelaney68 <mdelaney@digiconasp.com>
2015-02-19 12:48:49 -0500

    Fixed filenmname type

--- src/install.sh
+++ src/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Make sure needed applications are installed
 echo "Installing tools"
 sudo apt-get install wget zip unzip
 sudo apt-get install zip 
@@ -18,16 +17,16 @@ then
 fi
 sudo unzip -o Demo-master.zip  -d ./tmp
 sudo cp -r ./tmp/Demo-master/src/railoinstall.txt ./
-sudo cp -r ./tmp/Demo-master/src/uninstall.txt ./
+sudo cp -r ./tmp/Demo-master/src/uninstall.sh ./
+sudo chmod 775 uninstall.sh
 
-# install Railo  - see railoinstall.txt for defaultsettings
 echo "==================="
 echo "  Installing Railo "
 echo "  Please Wait!! - This may take several minutes"
 echo "==================="
 sudo ./railo-4.2.1.008-pl0-linux-x64-installer.run --mode unattended --optionfile railoinstall.txt
 sudo chmod 777 /opt/railo/ -R
-# install Demo-master into Railo website root
+
 echo "Installing Demo-master "
 sudo cp -r ./tmp/Demo-master/src/wwwroot /opt/railo/tomcat/webapps/ROOT/
 sudo rm ./tmp -R
